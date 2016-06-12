ed1bcb2728eccf455d535b47ba5c9cbf1dd2a361
Jamie Hand <hand.jamie@gmail.com>
2016-06-12 15:23:07 -0700

    Work on finding links

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,69 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
+	 *
 	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
+	 *
 	 * 1. Clicking on the first non-parenthesized, non-italicized link
      * 2. Ignoring external links, links to the current page, or red links
      * 3. Stopping when reaching "Philosophy", a page with no links or a page
      *    that does not exist, or when a loop occurs
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		
+
         // some example code to get you started
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+		// parse into paragraphs
 		Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		url = getFirstValidLink(paragraphs);
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		// Element firstPara = paragraphs.get(0);
+		//
+		// Iterable<Node> iter = new WikiNodeIterable(firstPara);
+		// for (Node node: iter) {
+		// 	// System.out.println("\nprinting");
+		// 	if (node instanceof TextNode) {
+		// 		System.out.print(node);
+		// 	}
+    // }
+
+    // the following throws an exception so the test fails
+    // until you update the code
+    String msg = "Complete this lab by adding your code and removing this statement.";
+    throw new UnsupportedOperationException(msg);
 	}
+
+	static String getFirstValidLink(Elements paragraphs) {
+		for (Element para: paragraphs) {
+			Iterable<Node> iter = new WikiNodeIterable(para);
+			for (Node node: iter) {
+				if (node.hasAttr("href")) {
+					// if (node instanceof TextNode) {
+						// System.out.print(node);
+						String absURL = node.attr("abs:href");
+						System.out.print(absURL);
+					// }
+				}
+
+				// Elements links = node.select("a");
+				// for (Element link: links) {
+				// 	System.out.print(link);
+				// }
+			}
+		}
+		return "asdf";
+	}
+
+
 }
